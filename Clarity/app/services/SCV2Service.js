6c1722f641e387ae262afd6ad16a95c457ebe8fe
ChristopherPendergraft <ChrisPendergraft@macs-imac-2.attlocal.net>
2018-05-15 16:48:35 -0700

    deleting unneed files

---
+++ Clarity/app/services/SCV2Service.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ChrisPendergraft on 5/5/16.
+ */
+angular.module('myApp.view2', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: 'view2/view2.html',
+            controller: 'DemoCtrl'
+        });
